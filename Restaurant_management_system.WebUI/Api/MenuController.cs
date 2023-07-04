ff5cecbcaf71ff314aed656e8c64155ebd1aaf31
Polovoy Vlad <vlad.polovoy@gmail.com>
2023-07-04 12:22:26 +0300

    Implemented console logger, azure logger

--- Restaurant_management_system.WebUI/Api/MenuController.cs
+++ Restaurant_management_system.WebUI/Api/MenuController.cs
@@ -2,12 +2,14 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
+using Microsoft.Docs.Samples;
 
 using Restaurant_management_system.WebUI.ApiModels;
 using Restaurant_management_system.Infrastructure.Data;
 using Restaurant_management_system.Core.DishesAggregate;
 using Restaurant_management_system.Core.TablesAggregate;
 using Restaurant_management_system.Core.Services.Attributes;
+using Restaurant_management_system.Core.Services.Logger;
 
 namespace Restaurant_management_system.WebUI.Api;
 
@@ -16,71 +18,73 @@ namespace Restaurant_management_system.WebUI.Api;
 [SwaggerControllerOrder(1)]
 public class MenuController : Controller
 {
-    private readonly ILogger<MenuController> _logger;
-    private readonly RestaurantContext _context;
-    private readonly IStringLocalizer<MenuController> _localizer;
+	private readonly ILogger<MenuController> _logger;
+	private readonly RestaurantContext _context;
+	private readonly IStringLocalizer<MenuController> _localizer;
 
-    public MenuController(ILogger<MenuController> logger, RestaurantContext context, IStringLocalizer<MenuController> localizer)
-    {
-        _logger = logger;
-        _context = context;
-        _localizer = localizer;
-    }
+	public MenuController(ILogger<MenuController> logger, RestaurantContext context, IStringLocalizer<MenuController> localizer)
+	{
+		_logger = logger;
+		_context = context;
+		_localizer = localizer;
+	}
 
-    /// <summary>
-    /// Get all dishes
-    /// </summary>
-    /// <returns></returns>
-    /// <remarks>
-    /// Sample request:
-    /// 
-    ///     GET api/menu
-    /// </remarks>
-    /// <response code="200">Returns all menu items</response>
-    /// <response code="204">Menu is empty</response>
-    // GET: api/menu
-    [HttpGet]
-    [ProducesResponseType(200)]
-    [ProducesResponseType(204)]
-    [Produces("application/json")]
-    public ActionResult<List<DishItemDTO>> GetMenu()
-    {
-        var dishes = _context.DishInMenu
-            .AsNoTracking()
-            .Select(d => new DishItemDTO
-            {
-                ID = d.ID,
-                Name = d.Name,
-                Price = d.Price
-            })
-            .ToList();
+	/// <summary>
+	/// Get all dishes
+	/// </summary>
+	/// <returns></returns>
+	/// <remarks>
+	/// Sample request:
+	/// 
+	///     GET api/menu
+	/// </remarks>
+	/// <response code="200">Returns all menu items</response>
+	/// <response code="204">Menu is empty</response>
+	// GET: api/menu
+	[HttpGet]
+	[ProducesResponseType(200)]
+	[ProducesResponseType(204)]
+	[Produces("application/json")]
+	public ActionResult<List<DishItemDTO>> GetMenu()
+	{
+		_logger.LogInformation(LogEvents.VisitMethod, "{route} visited at {time} by {user}. LogEvent:{logevent}", ControllerContext.ToCtxString(), DateTime.UtcNow.ToString(), User.Identity!.Name, LogEvents.VisitMethod);
 
-        if (dishes.Count == 0)
-            return NoContent();
+		var dishes = _context.DishInMenu
+			.AsNoTracking()
+			.Select(d => new DishItemDTO
+			{
+				ID = d.ID,
+				Name = d.Name,
+				Price = d.Price
+			})
+			.ToList();
 
-        // Ingredients:
-        foreach (var oneMenuEntity in dishes)
-        {
-            var ingredientsID = _context.IngredientForDishInMenu
-                .AsNoTracking()
-                .Where(i => i.DishInMenuID == oneMenuEntity.ID)
-                .ToList();
+		if (dishes.Count == 0)
+			return NoContent();
 
-            if (oneMenuEntity.Ingredients != null)
-                foreach (var oneMenuInredientsEntity in ingredientsID)
-                {
-                    var ingredient = _context.Ingredient
-                        .AsNoTracking()
-                        .FirstOrDefault(i => i.ID == oneMenuInredientsEntity.IngredientID);
-                    if (ingredient == null)
-                        continue;
+		// Ingredients:
+		foreach (var oneMenuEntity in dishes)
+		{
+			var ingredientsID = _context.IngredientForDishInMenu
+				.AsNoTracking()
+				.Where(i => i.DishInMenuID == oneMenuEntity.ID)
+				.ToList();
 
-                    oneMenuEntity.Ingredients.Add(ingredient.Name);
-                }
-        }
+			if (oneMenuEntity.Ingredients != null)
+				foreach (var oneMenuInredientsEntity in ingredientsID)
+				{
+					var ingredient = _context.Ingredient
+						.AsNoTracking()
+						.FirstOrDefault(i => i.ID == oneMenuInredientsEntity.IngredientID);
+					if (ingredient == null)
+						continue;
 
-        return Ok(dishes);
-    }
+					oneMenuEntity.Ingredients.Add(ingredient.Name);
+				}
+		}
+
+		return Ok(dishes);
+	}
 }
 
 //// <remarks>
--- Restaurant_management_system.WebUI/Api/OrderController.cs
+++ Restaurant_management_system.WebUI/Api/OrderController.cs
@@ -2,13 +2,14 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Localization;
+using Microsoft.Docs.Samples;
 
 using Restaurant_management_system.WebUI.ApiModels;
 using Restaurant_management_system.Infrastructure.Data;
 using Restaurant_management_system.Core.DishesAggregate;
 using Restaurant_management_system.Core.TablesAggregate;
 using Restaurant_management_system.Core.Services.Attributes;
-using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using Restaurant_management_system.Core.Services.Logger;
 
 namespace Restaurant_management_system.WebUI.Api;
 
@@ -17,81 +18,95 @@ namespace Restaurant_management_system.WebUI.Api;
 [SwaggerControllerOrder(2)]
 public class OrderController : Controller
 {
-    private readonly ILogger<OrderController> _logger;
-    private readonly RestaurantContext _context;
-    private readonly IStringLocalizer<OrderController> _localizer;
+	private readonly ILogger<OrderController> _logger;
+	private readonly RestaurantContext _context;
+	private readonly IStringLocalizer<OrderController> _localizer;
 
-    public OrderController(ILogger<OrderController> logger, RestaurantContext context, IStringLocalizer<OrderController> localizer)
-    {
-        _logger = logger;
-        _context = context;
-        _localizer = localizer;
-    }
+	public OrderController(ILogger<OrderController> logger, RestaurantContext context, IStringLocalizer<OrderController> localizer)
+	{
+		_logger = logger;
+		_context = context;
+		_localizer = localizer;
+	}
 
-    /// <summary>
-    /// Open new order for the specified table
-    /// </summary>
-    /// <returns></returns>
-    /// <remarks>
-    /// Sample request:
-    /// 
-    ///     PUT api/order/5
-    /// </remarks>
-    /// <param name="tableID"></param>
-    /// <response code="404">Table not found</response>
-    /// <response code="201">Returns the orderID of created order. Order connected to given tableID</response>
-    /// <response code="500">Server error</response>
-    // PUT: api/order/5
-    [HttpPut("{tableID}")]
-    [ProducesResponseType(404)]
-    [ProducesResponseType(201)]
-    [ProducesResponseType(500)]
-    [Produces("application/json")]
-    public ActionResult<int> CreateOrder(int tableID)
-    {
-        var table = _context.Table
-            .Find(tableID);
-        if (table == null)
-            return NotFound($"Table {tableID} not found");
+	/// <summary>
+	/// Open new order for the specified table
+	/// </summary>
+	/// <returns></returns>
+	/// <remarks>
+	/// Sample request:
+	/// 
+	///     PUT api/order/5
+	/// </remarks>
+	/// <param name="tableID"></param>
+	/// <response code="404">Table not found</response>
+	/// <response code="201">Returns the orderID of created order. Order connected to given tableID</response>
+	/// <response code="500">Server error</response>
+	// PUT: api/order/5
+	[HttpPut("{tableID}")]
+	[ProducesResponseType(404)]
+	[ProducesResponseType(201)]
+	[ProducesResponseType(500)]
+	[Produces("application/json")]
+	public ActionResult<int> CreateOrder(int tableID)
+	{
+		_logger.LogInformation(LogEvents.VisitMethod, "{route} visited at {time} by {user}. LogEvent:{logevent}", ControllerContext.ToCtxString(), DateTime.UtcNow.ToString(), User.Identity!.Name, LogEvents.VisitMethod);
 
-        var newOrder = new OrderInTableEntity() { TableID = tableID, SelfOrdered = true };
-        _context.OrderInTable.Add(newOrder);
-        _context.SaveChanges();
-        _context.Entry(newOrder).GetDatabaseValues();
+		var table = _context.Table
+			.Find(tableID);
+		if (table == null)
+		{
+			_logger.LogInformation(LogEvents.NotFoundInDB, "Item {item} not found in Table table. User:{user} LogEvent:{logevent}", tableID, User.Identity!.Name, LogEvents.NotFoundInDB);
+			return NotFound($"Table {tableID} not found");
+		}
 
-        if (newOrder.ID == 0)
-            return StatusCode(500, "Server error");
+		var newOrder = new OrderInTableEntity() { TableID = tableID, SelfOrdered = true };
+		_context.OrderInTable.Add(newOrder);
+		_context.SaveChanges();
+		_context.Entry(newOrder).GetDatabaseValues();
 
-        return Created("", newOrder.ID);
-    }
+		if (newOrder.ID == 0)
+			return StatusCode(500, "Server error");
 
-    /// <summary>
-    /// Close order
-    /// </summary>
-    /// <returns></returns>
-    /// <remarks>
-    /// Sample request:
-    /// 
-    ///     PATCH api/order/4
-    /// </remarks>
-    /// <param name="orderID"></param>
-    /// <response code="204">Order closed successfuly</response>
-    /// <response code="404">Order not found</response>
-    // PATCH: api/order/4
-    [HttpPatch("{orderID}")]
-    [ProducesResponseType(204)]
-    [ProducesResponseType(404)]
-    [Produces("application/json")]
-    public ActionResult CloseOrder(int orderID)
-    {
-        var order = _context.OrderInTable
-            .Find(orderID);
-        if (order == null)
-            return NotFound($"Order {orderID} not found");
+		_logger.LogInformation(LogEvents.CreateInDB, "Item {item} created in OrderInTable table. User:{user} LogEvent:{logevent}", newOrder.ID, User.Identity!.Name, LogEvents.CreateInDB);
 
-        order.Open = false;
+		return Created("", newOrder.ID);
+	}
 
-        return NoContent();
-    }
+	/// <summary>
+	/// Close order
+	/// </summary>
+	/// <returns></returns>
+	/// <remarks>
+	/// Sample request:
+	/// 
+	///     PATCH api/order/4
+	/// </remarks>
+	/// <param name="orderID"></param>
+	/// <response code="204">Order closed successfuly</response>
+	/// <response code="404">Order not found</response>
+	// PATCH: api/order/4
+	[HttpPatch("{orderID}")]
+	[ProducesResponseType(204)]
+	[ProducesResponseType(404)]
+	[Produces("application/json")]
+	public ActionResult CloseOrder(int orderID)
+	{
+		_logger.LogInformation(LogEvents.VisitMethod, "{route} visited at {time} by {user}. LogEvent:{logevent}", ControllerContext.ToCtxString(), DateTime.UtcNow.ToString(), User.Identity!.Name, LogEvents.VisitMethod);
+
+		var order = _context.OrderInTable
+			.Find(orderID);
+		if (order == null)
+		{
+			_logger.LogInformation(LogEvents.NotFoundInDB, "Item {item} not found in OrderInTable table. User:{user} LogEvent:{logevent}", orderID, User.Identity!.Name, LogEvents.NotFoundInDB);
+			return NotFound($"Order {orderID} not found");
+		}
+
+		order.Open = false;
+		_context.SaveChanges();
+
+		_logger.LogInformation(LogEvents.SetDataInDB, "Item {item} changed in OrderInTable table. User:{user} LogEvent:{logevent}", orderID, User.Identity!.Name, LogEvents.SetDataInDB);
+
+		return NoContent();
+	}
 }
-
