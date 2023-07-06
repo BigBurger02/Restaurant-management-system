c1780ba30f15cfd84a06ebf5d8f69bfb7f29d53b
Polovoy Vlad <vlad.polovoy@gmail.com>
2023-07-06 19:52:11 +0300

    Added DuendeIdentityServer as independent project

---
+++ IdentityServer/Config.cs
@@ -0,0 +1,58 @@
+﻿using IdentityModel;
+using Duende.IdentityServer;
+using Duende.IdentityServer.Models;
+
+namespace IdentityServer;
+
+public static class Config
+{
+	public static IEnumerable<IdentityResource> IdentityResources =>
+		new List<IdentityResource>
+		{
+			new IdentityResources.OpenId(),
+			new IdentityResources.Profile(),
+			new IdentityResource()
+			{
+				Name = "email",
+				UserClaims = new List<string>
+				{
+					JwtClaimTypes.Email,
+					JwtClaimTypes.EmailVerified
+				}
+			},
+		};
+
+
+	public static IEnumerable<ApiScope> ApiScopes =>
+		new List<ApiScope>
+		{
+			new ApiScope("api", "RestaurantAPI")
+		};
+
+	public static IEnumerable<Client> Clients;
+
+	public static IEnumerable<Client> SetSecrets(IConfigurationSection config)
+	{
+		Clients = new List<Client>
+		{
+			new Client
+			{
+				ClientId = "webapp1",
+				ClientSecrets = { new Secret(config.GetValue<string>("webapp1:secret").Sha256()) },
+				AllowedGrantTypes = GrantTypes.CodeAndClientCredentials,
+				RedirectUris = { "https://" + config.GetValue<string>("webapp1:uri") + "/signin-oidc" }, // https://localhost:9000/signin-oidc
+				PostLogoutRedirectUris = { "https://" + config.GetValue<string>("webapp1:uri") + "/signout-callback-oidc" }, // https://localhost:9000/signout-callback-oidc
+				AllowOfflineAccess = true,
+				AllowedScopes =
+				{
+					IdentityServerConstants.StandardScopes.OpenId,
+					IdentityServerConstants.StandardScopes.Profile,
+					"api",
+					"email",
+				}
+			},
+		};
+
+		return Clients;
+	}
+}
---
+++ Restaurant_management_system.WebUI/Api/IdentityController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Restaurant_management_system.WebUI.Api
+{
+	[Route("identity")]
+	[Authorize]
+	public class IdentityController : Controller
+	{
+		[HttpGet]
+		public IActionResult Get()
+		{
+			return new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+		}
+	}
+}
