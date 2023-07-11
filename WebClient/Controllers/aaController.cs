93239c1a6068a5bf354a2beb81186a26dc5b4989
Vladislav <104977243+BigBurger02@users.noreply.github.com>
2023-07-11 10:35:34 +0300

    bugfix (#35)

--- WebClient/Controllers/aaController.cs
+++ WebClient/Controllers/aaController.cs
@@ -27,11 +27,11 @@ namespace WebClient.Controllers
 		[HttpGet]
 		public async Task<IActionResult> checktokenfromclient()
 		{
-			var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
-			if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
-			{
-				return NotFound();
-			}
+			//var localAddresses = new string[] { "127.0.0.1", "::1", HttpContext.Connection.LocalIpAddress.ToString() };
+			//if (!localAddresses.Contains(HttpContext.Connection.RemoteIpAddress.ToString()))
+			//{
+			//	return NotFound();
+			//}
 
 			var userInfo = new UserInfo(await HttpContext.AuthenticateAsync());
 
