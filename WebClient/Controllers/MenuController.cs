ec68d2cef3d01e9318342da7ea9ce4d42d013431
Polovoy Vlad <vlad.polovoy@gmail.com>
2023-07-14 23:34:10 +0300

    Added adility to get menu, add dishes in cart. Added custom cookie

--- WebClient/Controllers/MenuController.cs
+++ WebClient/Controllers/MenuController.cs
@@ -1,18 +1,134 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using System.Net.Http.Headers;
+using WebClient.Models;
 using System.Text.Json;
 
-namespace WebClient.Controllers
-{
-	public class MenuController : Controller
-	{
+namespace WebClient.Controllers;
 
+[Authorize]
+public class MenuController : Controller
+{
+	private readonly IConfigurationSection ConfigSection;
+	private readonly string ISUri; // IdentityServer Uri
+	private readonly string ApiUri; //Api Uri
+	private readonly int TableID;
+
+	public MenuController(IConfiguration configRoot)
+	{
+		ConfigSection = configRoot.GetSection("ClientSecrets");
+		ISUri = "https://" + ConfigSection.GetValue<string>("IdentityServer:uri") + "/";
+		ApiUri = "https://" + ConfigSection.GetValue<string>("Api:uri") + "/";
+		TableID = 1;
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetAllMenu()
+	{
+		var accessToken = await HttpContext.GetTokenAsync("access_token");
+		var client = new HttpClient();
+		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+		var content = await client.GetStringAsync(ApiUri + "api/menu");
+
+		IEnumerable<MenuDTO> dishes = JsonSerializer.Deserialize<List<MenuDTO>>(content)!;
+
+		var orderID = ReadOrderIDCookie();
+		if (orderID != 0)
+		{
+			ViewData["orderID"] = orderID;
+		}
+
+		return View(dishes);
+	}
+
+	[HttpGet]
+	public async Task<IActionResult> GetCart()
+	{
+		var accessToken = await HttpContext.GetTokenAsync("access_token");
+		var client = new HttpClient();
+		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+		var content = await client.GetStringAsync(ApiUri + "api/menu");
+
+		IEnumerable<MenuDTO> dishes = JsonSerializer.Deserialize<List<MenuDTO>>(content)!;
+
+		var orderID = ReadOrderIDCookie();
+		if (orderID != 0)
+		{
+			ViewData["orderID"] = orderID;
+		}
+
+		return View(dishes);
+	}
+
+	public async Task<int> CreateOrder()
+	{
+		var accessToken = await HttpContext.GetTokenAsync("access_token");
+		var httpClient = new HttpClient(new HttpClientHandler())
+		{
+			BaseAddress = new Uri(ApiUri + "api/order/" + TableID)
+		};
+		httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+
+		string responseBody;
+		using (var response = httpClient.PostAsync(ApiUri + "api/order/" + TableID, null))
+		{
+			responseBody = response.Result.Content.ReadAsStringAsync().Result;
+		}
+
+		if (int.TryParse(responseBody, out int orderID))
+		{
+			SetOrderIDCookie(orderID);
+			return orderID;
+		}
+
+		return 0;
+	}
+
+	public async Task<IActionResult> AddToCart(int dishID)
+	{
+		var orderID = ReadOrderIDCookie();
+		if (orderID == 0)
+		{
+			orderID = await CreateOrder();
+			if (orderID == 0)
+				return StatusCode(500, "Server error");
+		}
+
+		var accessToken = await HttpContext.GetTokenAsync("access_token");
+		var client = new HttpClient();
+		client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", accessToken);
+		var result = await client.PostAsync(ApiUri + "api/cart/" + orderID + "/" + dishID, null);
+		if (result.IsSuccessStatusCode)
+			return RedirectToAction("GetAllMenu");
+		else
+			return StatusCode(500, "Server error");
+	}
+
+
+
+	private int ReadOrderIDCookie()
+	{
+		if (int.TryParse(Request.Cookies["OrderID"], out int orderID))
+			return orderID;
+		return 0;
+	}
+	private void SetOrderIDCookie(int orderID)
+	{
+		var cookieOptions = new CookieOptions();
+		cookieOptions.Expires = DateTime.Now.AddMinutes(2);
+		cookieOptions.Path = "/";
+		Response.Cookies.Append("OrderID", orderID.ToString(), cookieOptions);
+	}
+	public IActionResult ResetOrderIDCookie()
+	{
+		var cookieOptions = new CookieOptions();
+		cookieOptions.Expires = DateTime.Now.AddSeconds(10);
+		cookieOptions.Path = "/";
+		Response.Cookies.Append("OrderID", "0", cookieOptions);
+
+		return RedirectToAction("GetAllMenu");
 	}
 }
-
